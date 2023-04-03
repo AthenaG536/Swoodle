52ea7e599ed511b10715a5b2a137aeff0421d367
Athena <athena536@hotmail.com>
2023-04-03 16:13:30 +1200

    letter block collision populates letter within Letter Slot 3
    Next: get the other letter slots to be populated on collision too

--- Assets/Scripts/letterCollision.cs
+++ Assets/Scripts/letterCollision.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using TMPro;
 
 public class letterCollision : MonoBehaviour
 {
+    public TMP_Text letterText;
+    public TMP_Text slotText;
+
     // public Transform 
     void OnCollisionEnter(Collision collisionInfo) {
         Debug.Log (collisionInfo.collider.name);
 
         if(collisionInfo.collider.tag == "Letter Slot"){
             Debug.Log("Letter hit a letter slot");
+            Debug.Log("The Letter was...");
+            Debug.Log(letterText.text);
+            slotText.text = letterText.text;
         }
 
     }
