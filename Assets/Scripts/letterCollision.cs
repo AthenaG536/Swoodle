55d7d1a5e3aca4146de03a41b6f6803151761ac8
Athena <athena536@hotmail.com>
2023-04-03 16:39:25 +1200

    Each Letter slot updates now

--- Assets/Scripts/letterCollision.cs
+++ Assets/Scripts/letterCollision.cs
@@ -5,6 +5,11 @@ public class letterCollision : MonoBehaviour
 {
     public TMP_Text letterText;
     public TMP_Text slotText;
+    public TMP_Text slot1;
+    public TMP_Text slot2;
+    public TMP_Text slot3;
+    public TMP_Text slot4;
+    public TMP_Text slot5;
 
     // public Transform 
     void OnCollisionEnter(Collision collisionInfo) {
@@ -12,9 +17,36 @@ public class letterCollision : MonoBehaviour
 
         if(collisionInfo.collider.tag == "Letter Slot"){
             Debug.Log("Letter hit a letter slot");
-            Debug.Log("The Letter was...");
-            Debug.Log(letterText.text);
-            slotText.text = letterText.text;
+            if(slotText && letterText){
+                slotText.text = letterText.text;
+            }
+
+            if(letterText){
+                string slotNumber = collisionInfo.collider.name.Substring(collisionInfo.collider.name.Length - 1);
+                switch(slotNumber) 
+                {
+                  case "1":
+                    slot1.text = letterText.text;
+                    break;
+                  case "2":
+                    slot2.text = letterText.text;
+                    break;
+                  case "3":
+                    slot3.text = letterText.text;
+                    break;
+                  case "4":
+                    slot4.text = letterText.text;
+                    break;
+                  case "5":
+                    slot5.text = letterText.text;
+                    break;
+                  default:
+                    break;
+                }
+
+
+            }
+            
         }
 
     }
